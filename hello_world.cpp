62b2a4a314bf0937cc6efe10acf217562d43c273
elys33ve <elys33ve@gmail.com>
2022-01-11 18:40:37 -0500

    hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>             // header file library--allows input/output objects
+using namespace std;            // allows use of names for objects and variables from the standard library
+                        //semicolon after each statement
+int main (){                    // main function call
+  cout << "hello world";        // cout--"see-out", <<--insertion operator
+  cout << "hello world" << endl;// end line/newline (or just \n)
+  return 0;                     // return 0--ends main function
+}                               // close function bracket
+
+//single line comment
+
+/* multi
+    line
+    comment */
