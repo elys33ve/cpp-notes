bfcb8e6d1a6ad50e5064542eac4bc7cd18605eb4
elys33ve <elys33ve@gmail.com>
2022-01-11 19:25:31 -0500

    variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>             // header file library--allows input/output objects
+using namespace std;            // allows use of names for objects and variables from the standard library
+
+int main (){                    // main function call
+  int intNum = 111;               //assign variable type (integer)
+  intNum = 11;                    //changes if assigned new value (now 11)
+  double floatNum = 5.555;        //float
+  char letter = 'g';              //character
+  string text = "asdfsad";        //string
+  bool truefalse = true;          //boolean
+
+  int age = 2000;
+  cout << "im " << age << " years old\n";     //combine
+  
+  int x = 5;
+  int y = 60;
+  int sum = x + y;
+  cout << sum;                            //math w variables
+
+  int s = 3, f = 6, n = 4;                //declare multi variables in one line
+
+  const int constantInt = 50;             //constant
+}
