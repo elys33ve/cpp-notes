ee315eea36ffd53e1afa8f19e3bca348e11e89f1
elys33ve <elys33ve@gmail.com>
2022-02-25 17:27:02 -0500

    vector_notes.cpp

---
+++ vector_notes.cpp
@@ -0,0 +1,24 @@
+// this says cpp file but it actually just notes on vector stuff
+
+#include <vector>
+
+vector<int> v;
+
+v.size()
+v.resize()
+v.at()
+
+v.push_back()               //append
+v.back()                    //last element
+v.pop_back()                //remove last element   
+
+for (i = v.size() - 1; i>=0; i--){  
+
+strcpy(x,y)                 //copies y to x
+strncpy(x,y,numchars)
+strcat(x,y)                 //copies to end
+strncat(x,y,numchars)
+
+strchr(y,searchchar)
+strlen(y)
+strcmp(str1,str2)           //return 0 if strs are equal
